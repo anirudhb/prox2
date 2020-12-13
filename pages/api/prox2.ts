56ea56b8f1d6e32e0291f97f79d88fd2b2a7ec81
anirudhb <anirudhb@users.noreply.github.com>
2020-12-13 14:38:47 -0800

    verify signature in slash command

--- pages/api/prox2.ts
+++ pages/api/prox2.ts
@@ -1,9 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import { validateData, failRequest, succeedRequest, stageConfession } from '../../lib/main';
+import { validateData, failRequest, succeedRequest, stageConfession, verifySignature, api_config, setupMiddlewares } from '../../lib/main';
+
+export const config = api_config;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    await setupMiddlewares(req, res);
+
     console.log(`Request!`);
+    console.log(`Verifying signature...`);
+    const isValid = verifySignature(req);
+    if (!isValid) {
+        console.log(`Invalid!`);
+        res.writeHead(400).end();
+        return;
+    }
+    console.log(`Valid!`);
     console.log(`Validating request...`);
     const data = await validateData(req);
     if (data == null) {
