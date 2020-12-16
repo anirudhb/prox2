ad3c154f659ac4bf8111275c13e12984028e2036
anirudhb <anirudhb@users.noreply.github.com>
2020-12-16 15:09:22 -0800

    trying a thing

--- pages/api/prox2.ts
+++ pages/api/prox2.ts
@@ -24,18 +24,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return;
     }
     console.log(`Acknowledging request...`);
-    res.writeHead(200).end();
+    res.writeHead(200);
     if (data.text.trim().length <= 0) {
         console.log(`Text is none, sending help!`);
+        res.end();
         return await failRequest(data.response_url, `Uh oh! Try again with a message you\'d like to confess!
 Tip: Draft your confession in a DM so others don\'t see that you\'re typing!`);
     }
     try {
         await stageConfession(data.text, data.user_id);
     } catch (e) {
+        res.end();
         return await failRequest(data.response_url, e);
     }
     console.log(`Notifying user...`);
     await succeedRequest(data.response_url, 'Your message has been staged and will appear here after review by the confessions team!');
     console.log(`Request success`);
+    res.end();
 }
