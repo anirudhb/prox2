a698c0d61a6aa2196da30f64612e8b7bb5a4badd
anirudhb <anirudhb@users.noreply.github.com>
2020-12-23 20:18:00 -0800

    add source maps to stacktraces

--- lib/main.ts
+++ lib/main.ts
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import 'source-map-support/register';
+
 import crypto from 'crypto';
 import https from 'https';
 
