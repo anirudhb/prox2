3a55092f587e0b3d1ce3e05077619c0e4188b546
anirudhb <anirudhb@users.noreply.github.com>
2021-03-15 11:15:23 -0700

    use as user

--- pages/api/events_work.ts
+++ pages/api/events_work.ts
@@ -14,66 +14,77 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import { api_config, setupMiddlewares, stageDMConfession, validateNonce, verifySignature } from '../../lib/main';
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  api_config,
+  setupMiddlewares,
+  stageDMConfession,
+  validateNonce,
+  verifySignature,
+} from "../../lib/main";
 
 export const config = api_config;
 
 interface UrlVerificationEvent {
-    type: 'url_verification';
-    token?: string;
-    challenge: string;
+  type: "url_verification";
+  token?: string;
+  challenge: string;
 }
 
 interface DMEvent {
-    type: 'message';
-    channel_type: 'im';
-    ts: string;
-    text: string;
-    user: string;
-    bot_profile?: {
-        app_id: string;
-    };
-    channel: string;
+  type: "message";
+  channel_type: "im";
+  ts: string;
+  text: string;
+  user: string;
+  bot_profile?: {
+    app_id: string;
+  };
+  channel: string;
 }
 
-export type SlackEventPayload = UrlVerificationEvent | {
-    type: 'event_callback';
-    event: DMEvent;
-};
+export type SlackEventPayload =
+  | UrlVerificationEvent
+  | {
+      type: "event_callback";
+      event: DMEvent;
+    };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    await setupMiddlewares(req, res);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await setupMiddlewares(req, res);
 
-    try {
-        await validateNonce(req);
-    } catch (e) {
-        console.log(e);
-        res.writeHead(400).end();
-        return;
-    }
+  try {
+    await validateNonce(req);
+  } catch (e) {
+    console.log(e);
+    res.writeHead(400).end();
+    return;
+  }
 
-    console.log(`Event!`);
-    console.log(`Validating signature...`);
-    const isValid = verifySignature(req);
-    if (!isValid) {
-        console.log(`Invalid!`);
-        res.writeHead(400).end();
-        return;
+  console.log(`Event!`);
+  console.log(`Validating signature...`);
+  const isValid = verifySignature(req);
+  if (!isValid) {
+    console.log(`Invalid!`);
+    res.writeHead(400).end();
+    return;
+  }
+  console.log(`Valid!`);
+  const payload = req.body as SlackEventPayload;
+  console.log(`Type = ${payload.type}`);
+  if (payload.type == "event_callback") {
+    const data = payload.event;
+    if (data.type == "message" && data.channel_type == "im") {
+      console.log("DM!");
+      if (!data.bot_profile) {
+        // Handle DM staging...
+        await stageDMConfession(data.ts, data.user);
+      }
     }
-    console.log(`Valid!`);
-    const payload = req.body as SlackEventPayload;
-    console.log(`Type = ${payload.type}`);
-    if (payload.type == 'event_callback') {
-        const data = payload.event;
-        if (data.type == 'message' && data.channel_type == 'im') {
-            console.log('DM!');
-            if (!data.bot_profile) {
-                // Handle DM staging...
-                await stageDMConfession(data.ts, data.user);
-            }
-        }
-    }
-    console.log(`Request success`);
-    res.writeHead(204).end();
+  }
+  console.log(`Request success`);
+  res.writeHead(204).end();
 }
