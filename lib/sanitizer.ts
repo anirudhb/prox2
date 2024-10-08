372ec43acbcbd81dda46a47d1c26b07c49f04af7
David <62346025+aboutdavid@users.noreply.github.com>
2024-10-08 12:30:36 -0400

    remove group pings + allow pings to look nicer

--- lib/sanitizer.ts
+++ lib/sanitizer.ts
@@ -1,4 +1,7 @@
 export function sanitize(message: string) {
   return message
+    .replace(/<@[^|]+\|([^>]+)>/g, "[$1]")
+    .replace(/<@.*?>/g, "[user]")
+    .replace(/!subteam\^.*?\b/g, "[group]")
     .replace(/<!(channel|here|everyone)>/g, "<redacted for mass ping risk>");
 }
